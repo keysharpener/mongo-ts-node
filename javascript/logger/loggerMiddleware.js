9917f073886bbe651450c472a78a739645f331a6
Robin Francillon <robin.francillon@gmail.com>
2020-04-02 02:44:04 +0200

    Removed javascript compilated folder

--- javascript/logger/loggerMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function loggerMiddleware(request, response, next) {
-    console.log(request.method + " " + request.path);
-    next();
-}
-exports.loggerMiddleware = loggerMiddleware;
